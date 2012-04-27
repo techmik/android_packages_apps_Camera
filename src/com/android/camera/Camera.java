f79337e0e80ce9a8c8eb3285b3c3c3eecf06c127
techmik <techmik67@gmail.com>
2012-04-27 11:05:52 -0400

    add camera timer
    based off Danesh's work on gingerbread branch camera
    ba6660fd12401bf7fe7aeb6f5c3196c603e7c1ae
    
    Change-Id: I7b84e0c978d86944adf1529797592fbf0a0ddeb6

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -1281,7 +1281,8 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
                 CameraSettings.KEY_POWER_SHUTTER,
                 CameraSettings.KEY_STORAGE,
                 CameraSettings.KEY_PICTURE_SIZE,
-                CameraSettings.KEY_FOCUS_MODE};
+                CameraSettings.KEY_FOCUS_MODE,
+                CameraSettings.KEY_CAPTURE_MODE};
 
         CameraPicker.setImageResourceId(R.drawable.ic_switch_photo_facing_holo_light);
         mIndicatorControlContainer.initialize(this, mPreferenceGroup,
